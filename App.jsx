a75c052d3775fcf2942cc6141edaebdd9f563086
shashipinkie <55661732+shashipinkie@users.noreply.github.com>
2021-06-05 12:47:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <h4> <Footer/></h4>
+     </div> 
+      
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Dev Bootcamp</h1>
+      <p>
+        Thank you Shaurya sir for delivering an amazing Bootcamp.We indeed
+        learnt a lot from you Thank you and Gos Bless!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
